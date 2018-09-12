7f7ef483d918e145afa85f06bb6d3ecd23e21afd
dragostinova <43222575+dragostinova@users.noreply.github.com>
2018-09-12 15:46:40 -0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.1/firebase-messaging.js');
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDND3NMm2BdZXTeVAtP3QBDsM7-xbNcba0",
-  authDomain: "web-le-74dbb.firebaseapp.com",
-  databaseURL: "https://web-le-74dbb.firebaseio.com",
-  projectId: "web-le-74dbb",
-  storageBucket: "web-le-74dbb.appspot.com",
-  messagingSenderId: "503063201337"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
